a797b662c042a0d3d7c7fef9e88fa128b783d824
Levi Starrett <levi@roxsoftware.com>
2014-11-01 14:35:29 -0400

    added error checking to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,19 +22,28 @@ bash scripts/package.sh
 
 # commit to master
 git add config/ dist/
+[ $? -eq 0 ] || abort
 git commit -m "$1"
+[ $? -eq 0 ] || abort
 git push origin master
+[ $? -eq 0 ] || abort
 
 # checkout gh-pages
 git checkout gh-pages
+[ $? -eq 0 ] || abort
 
 # move the new distribution
 git show master:dist/dot.tgz > dot.tgz
+[ $? -eq 0 ] || abort
 
 # commit
 git add .
+[ $? -eq 0 ] || abort
 git commit -m "Updated distribution from `git show-ref -s --abbrev --heads master`"
+[ $? -eq 0 ] || abort
 git push origin gh-pages
+[ $? -eq 0 ] || abort
 
 # checkout master
 git checkout master
+[ $? -eq 0 ] || abort
