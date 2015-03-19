a47b40d13a27b46f5874ceaa6ff7bd48c4168294
Levi Starrett <levi@roxsoftware.com>
2015-03-19 12:15:23 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 
 ACTION="NONE"
 
+# remove log file
+rm -f ~/.dot.log
+
 # Check arguments
 if [ $# -gt 1 ]; then
     echo "-dot: ERROR: Too many arguments"
@@ -19,44 +22,52 @@ else
 fi
 
 # INSTALL
-if [ $ACTION -eq "INSTALL" ]; then
+if [ $ACTION == "INSTALL" ]; then
 
     echo "-dot: Installing dot files..."
 
     cd ~
-    git clone git@github.com:leviathan747/.dot.git &>> ~/dot.log
+    [ -d .dot ] && rm -rf .dot >> ~/.dot.log 2>&1
+    git clone git@github.com:leviathan747/.dot.git >> ~/.dot.log 2>&1
     if [ $? -ne 0 ]; then
         echo "-dot: ERROR: Git clone failed. See full log in ~/dot.log"
         exit 1
     fi
 
-    cd .dot/
+    cd ~/.dot/
 
     # Make backups
-    [ -d backup ] && rm -rf backup
+    [ -d backup ] && rm -rf backup >> ~/.dot.log 2>&1
     mkdir backup
 
-    [ -f ~/.bash_aliases ] && cp -f ~/.bash_aliases backup/bash_aliases
-    [ -f ~/.bashlinux ] && cp -f ~/.bashlinux backup/bashlinux
-    [ -f ~/.bashmac ] && cp -f ~/.bashmac backup/bashmac
-    [ -f ~/.bashrc ] && cp -f ~/.bashrc backup/bashrc
-    [ -f ~/.gitconfig ] && cp -f ~/.gitconfig backup/gitconfig
-    [ -f ~/.gitignore ] && cp -f ~/.gitignore backup/gitignore
-    [ -f ~/.vimrc ] && cp -f ~/.vimrc backup/vimrc
-    [ -d ~/.vim ] && cp -rf ~/.vim backup/vim
+    [ -f ~/.bash_aliases ] && cp -f ~/.bash_aliases backup/bash_aliases >> ~/.dot.log 2>&1
+    [ -f ~/.bashlinux ] && cp -f ~/.bashlinux backup/bashlinux >> ~/.dot.log 2>&1
+    [ -f ~/.bashmac ] && cp -f ~/.bashmac backup/bashmac >> ~/.dot.log 2>&1
+    [ -f ~/.bashrc ] && cp -f ~/.bashrc backup/bashrc >> ~/.dot.log 2>&1
+    [ -f ~/.gitconfig ] && cp -f ~/.gitconfig backup/gitconfig >> ~/.dot.log 2>&1
+    [ -f ~/.gitignore ] && cp -f ~/.gitignore backup/gitignore >> ~/.dot.log 2>&1
+    [ -f ~/.vimrc ] && cp -f ~/.vimrc backup/vimrc >> ~/.dot.log 2>&1
+    [ -d ~/.vim ] && cp -rf ~/.vim backup/vim >> ~/.dot.log 2>&1
+
+    # remove old files
+    rm -rf ~/.bash_aliases ~/.bashlinux ~/.bashmac ~/.bashrc ~/.gitconfig ~/.gitignore ~/.vimrc ~/.vim >> ~/.dot.log 2>&1
 
     # Link files
-    ln -s config/bash_aliases ~/.bash_aliases
-    ln -s config/bashlinux ~/.bashlinux
-    ln -s config/bashmac ~/.bashmac
-    ln -s config/bashrc ~/.bashrc
-    ln -s config/gitconfig ~/.gitconfig
-    ln -s config/gitignore ~/.gitignore
-    ln -s config/vimrc ~/.vimrc
-    ln -s config/vim ~/.vim
+    ln -s ~/.dot/config/bash_aliases ~/.bash_aliases >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/bashlinux ~/.bashlinux >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/bashmac ~/.bashmac >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/bashrc ~/.bashrc >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/gitconfig ~/.gitconfig >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/gitignore ~/.gitignore >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/vimrc ~/.vimrc >> ~/.dot.log 2>&1
+    ln -s ~/.dot/config/vim ~/.vim >> ~/.dot.log 2>&1
+
+    # Pull down submodules
+    git submodule init >> ~/.dot.log 2>&1
+    git submodule update >> ~/.dot.log 2>&1
 
     # Source bashrc
-    . ~/.bashrc
+    . ~/.bashrc >> ~/.dot.log 2>&1
 
     echo "-dot: Done."
 
