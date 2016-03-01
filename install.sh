dae959a23b3d719bf962f4cc39dcb16f237038ba
Levi Starrett <levi@roxsoftware.com>
2016-03-01 10:58:17 -0500

    added simplified install script

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# DOT FILE SETUP SCRIPT
+
+LOGFILE="dot.log"
+
+# move to .dot directory
+cd ~/.dot
+
+# remove log file
+rm -f dot.log
+
+# INSTALL
+if [ $ACTION == "INSTALL" ]; then
+
+    echo "-dot: Installing dot files..."
+
+    # Make backups
+    echo "-dot: Making backups..."
+    [ -d backup ] && rm -rf backup >> LOGFILE 2>&1
+    mkdir backup
+
+    [ -f ~/.bash_aliases ] && cp -f ~/.bash_aliases backup/bash_aliases >> LOGFILE 2>&1
+    [ -f ~/.bashlinux ] && cp -f ~/.bashlinux backup/bashlinux >> LOGFILE 2>&1
+    [ -f ~/.bashmac ] && cp -f ~/.bashmac backup/bashmac >> LOGFILE 2>&1
+    [ -f ~/.bashrc ] && cp -f ~/.bashrc backup/bashrc >> LOGFILE 2>&1
+    [ -f ~/.profile ] && cp -f ~/.profile backup/profile >> LOGFILE 2>&1
+    [ -f ~/.gitconfig ] && cp -f ~/.gitconfig backup/gitconfig >> LOGFILE 2>&1
+    [ -f ~/.gitignore ] && cp -f ~/.gitignore backup/gitignore >> LOGFILE 2>&1
+    [ -f ~/.vimrc ] && cp -f ~/.vimrc backup/vimrc >> LOGFILE 2>&1
+    [ -d ~/.vim ] && cp -rf ~/.vim backup/vim >> LOGFILE 2>&1
+
+    # remove old files
+    rm -rf ~/.bash_aliases ~/.bashlinux ~/.bashmac ~/.bashrc ~/.profile ~/.gitconfig ~/.gitignore ~/.vimrc ~/.vim >> LOGFILE 2>&1
+
+    # Link files
+    echo "-dot: Linking config files..."
+    ln -s ~/.dot/config/bash_aliases ~/.bash_aliases >> LOGFILE 2>&1
+    ln -s ~/.dot/config/bashlinux ~/.bashlinux >> LOGFILE 2>&1
+    ln -s ~/.dot/config/bashmac ~/.bashmac >> LOGFILE 2>&1
+    ln -s ~/.dot/config/bashrc ~/.bashrc >> LOGFILE 2>&1
+    ln -s ~/.dot/config/profile ~/.profile >> LOGFILE 2>&1
+    ln -s ~/.dot/config/gitconfig ~/.gitconfig >> LOGFILE 2>&1
+    ln -s ~/.dot/config/gitignore ~/.gitignore >> LOGFILE 2>&1
+    ln -s ~/.dot/config/vimrc ~/.vimrc >> LOGFILE 2>&1
+    ln -s ~/.dot/config/vim ~/.vim >> LOGFILE 2>&1
+
+    # Pull down submodules
+    echo "-dot: Updating submodules..."
+    git submodule init >> LOGFILE 2>&1
+    git submodule update >> LOGFILE 2>&1
+
+    echo "-dot: Done."
+
+fi
